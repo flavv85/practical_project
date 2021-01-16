b6049965e0c927aab6fcb00125d25a43d643b1b6
Flavius_Gorgan <gorganflavius@gmail.com>
2021-01-16 10:55:32 +0200

    - added pom.xml file for Maven configuration

---
+++ src/main/java/DBConfig.java
@@ -0,0 +1,16 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DBConfig {
+
+    private SessionFactory sessionFactory;
+
+    public DBConfig() {
+
+        Configuration configuration = new Configuration();
+
+
+
+    }
+
+}
